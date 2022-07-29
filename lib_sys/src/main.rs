9d84ce9699b9679c8181085bb922c520d8e8d74d
りょー <94099895+rkndayo@users.noreply.github.com>
2022-07-29 12:31:37 +0900

    aaaa

--- lib_sys/src/main.rs
+++ lib_sys/src/main.rs
@@ -19,9 +19,17 @@ fn main() -> Result<()> {
         "青空文庫",
         1600,
     ])?;
-    println!("1 テーブル作成");
-    println!("2 レコード追加");
+    stmt.execute(params![
+        10,
+        "ノルウェーの森",
+        "村上春樹",
+        459,
+        "幻冬舎",
+        1400,
+    ])?;
     loop {
+        println!("1 テーブル作成");
+        println!("2 レコード追加");
         // ........
         let mut guess = String::new();
         io::stdin()
@@ -50,27 +58,96 @@ fn main() -> Result<()> {
             io::stdin().read_line(&mut number_id).ok();
             let id:i32 = number_id.trim().parse().ok().unwrap();
 
-            println!("タイトルを入力してください。");
+            println!("タイトルを修正しますか?y/n");
+            let mut t = String::new();
+            io::stdin().read_line(&mut t).ok();
             let mut title = String::new();
-            io::stdin().read_line(&mut title).ok();
+            if t.trim() == "y" {
+                println!("タイトルを入力してください。");
+                io::stdin().read_line(&mut title).ok();
+            } else {
+                let se = format!("select title from book where id = {}", id);
+                let mut stmt = cn.prepare(&se)?;
+                let mut rows = stmt.query(params![])?;
 
-            println!("著者を入力してください。");
+                while let Some(row) = rows.next()?{
+                    title = row.get(0)?;
+                }
+            }
+
+            println!("著者を修正しますか?y/n");
+            let mut a = String::new();
+            io::stdin().read_line(&mut a).ok();
             let mut auther = String::new();
-            io::stdin().read_line(&mut auther).ok();
+            if a.trim() == "y" {
+                println!("著者を入力してください。");
+                io::stdin().read_line(&mut auther).ok();
+            } else {
+                let se = format!("select auther from book where id = {}", id);
+                let mut stmt = cn.prepare(&se)?;
+                let mut rows = stmt.query(params![])?;
 
-            println!("ページ数を入力してください。");
+                while let Some(row) = rows.next()?{
+                    auther = row.get(0)?;
+                }
+            }
+
+            println!("ページ数を修正しますか?y/n");
+            let mut p = String::new();
+            io::stdin().read_line(&mut p).ok();
             let mut number_page = String::new();
-            io::stdin().read_line(&mut number_page).ok();
-            let page:i32 = number_page.trim().parse().ok().unwrap();
+            let mut page:i32 = 0;
 
-            println!("出版社を入力してください。");
+            if p.trim() == "y" {
+                println!("ページ数を入力してください。");
+                io::stdin().read_line(&mut number_page).ok();
+                page = number_page.trim().parse().ok().unwrap();
+            } else {
+                let se = format!("select page from book where id = {}", id);
+                let mut stmt = cn.prepare(&se)?;
+                let mut rows = stmt.query(params![])?;
+
+                while let Some(row) = rows.next()?{
+                    page = row.get(0)?;
+                }
+            }
+
+            println!("出版社を修正しますか?y/n");
+            let mut p = String::new();
+            io::stdin().read_line(&mut p).ok();
             let mut publisher = String::new();
-            io::stdin().read_line(&mut publisher).ok(); 
+            if p.trim() == "y" {
+                println!("出版社を入力してください。");
+                io::stdin().read_line(&mut publisher).ok(); 
+            } else {
+                let se = format!("select publisher from book where id = {}", id);
+                let mut stmt = cn.prepare(&se)?;
+                let mut rows = stmt.query(params![])?;
 
-            println!("値段を入力してください。");
+                while let Some(row) = rows.next()?{
+                    publisher = row.get(0)?;
+                }
+            }
+
+            println!("値段を修正しますか?y/n");
+            let mut p = String::new();
+            io::stdin().read_line(&mut p).ok();
             let mut number_price = String::new();
-            io::stdin().read_line(&mut number_price).ok(); 
-            let price:i32 = number_price.trim().parse().ok().unwrap();
+            let mut price: i32 = 0;
+
+            if p.trim() == "y" {
+                println!("値段を入力してください。");
+                io::stdin().read_line(&mut number_price).ok(); 
+                price = number_price.trim().parse().ok().unwrap();
+            } else {
+                let se = format!("select price from book where id = {}", id);
+                let mut stmt = cn.prepare(&se)?;
+                let mut rows = stmt.query(params![])?;
+
+                while let Some(row) = rows.next()?{
+                    price = row.get(0)?;
+                }
+            }
 
             let s = format!("update book set title = '{}', auther = '{}', page = '{}', publisher = '{}', price = '{}' where id = {}", title, auther, page, publisher, price, id);
             cn.execute(&s, params![])?;
