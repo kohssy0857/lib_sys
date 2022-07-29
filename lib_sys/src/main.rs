14b874b8c302077d28dc536a817807ad7d95b7d6
tachi002 <82631748+tachi002@users.noreply.github.com>
2022-07-29 10:22:18 +0900

    1,2,3,4

--- lib_sys/src/main.rs
+++ lib_sys/src/main.rs
@@ -91,14 +91,120 @@ fn main() -> Result<()> {
           }
 
         } else if &guess.trim() == &"3" {
-            let mut guess = String::new();
-            io::stdin().read_line(&mut guess).expect("Failed to read line.");
+             println!("値を検索するカラムを選択してください");
+            println!("1:id");
+            println!("2:タイトル");
+            println!("3:作者");
+            println!("4:ページ数");
+            println!("5:出版社");
+            println!("6:価格");
+
+            let mut col = String::new();
+            io::stdin()
+                .read_line(&mut col)
+                .expect("Failed to read line.");
+            let col = col.trim();
+            println!("検索したい文字列、値を入力してください");
+            let mut find = String::new();
+            io::stdin()
+                .read_line(&mut find)
+                .expect("Failed to read line.");
+            let find = find.trim();
+            if col == "1" {
+                let mut n = cn.prepare(r"SELECT * FROM book where id == ? ")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            } else if col == "2" {
+                let mut n = cn.prepare(r"SELECT * FROM book where title LIKE '%?%'")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            } else if col == "3" {
+                let mut n = cn.prepare(r"SELECT * FROM book where auther LIKE '%?%' ")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            } else if col == "4" {
+                let mut n = cn.prepare(r"SELECT * FROM book where page ==? ")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            } else if col == "5" {
+                let mut n = cn.prepare(r"SELECT * FROM book where publisher LIKE '%?%' ")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            } else if col == "6" {
+                let mut n = cn.prepare(r"SELECT * FROM book where price== ?% ")?;
+                let iter = n.query_map(params![&find], |row| {
+                    Ok(Book {
+                        id: row.get(0)?,
+                        title: row.get(1)?,
+                        auther: row.get(2)?,
+                        page: row.get(3)?,
+                        publisher: row.get(4)?,
+                        price: row.get(5)?,
+                    })
+                })?;
+                for it in iter {
+                    println!("{:?}", it);
+                }
+            }
         } else if &guess.trim() == &"4" {
           loop{
             println!("↓↓");
             println!("↓↓");
-            println!("idを入力してください");
-            println!("00:終了");
             let mut stmt = cn.prepare("select * from book")?;
             // クロージャを使用してデータを取得して構造体 book にセット
             for it in stmt.query_map(params![], |row| {
@@ -114,6 +220,8 @@ fn main() -> Result<()> {
               // 表示
               println!("{:?}", it.unwrap());
             }
+            println!("00:終了");
+            println!("idを入力してください");
             let mut num = String::new();
             // 入力
             io::stdin().read_line(&mut num).ok();
